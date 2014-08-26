9b4b9c88331a14164061f64f100b93d02a2d6026
Elger van Boxtel <elgervb@gmail.com>
2014-08-26 12:10:29 +0200

    baseurl, version & comments
    BaseUrl is now a setting to be supplied when creating a new Backend.Api
    Exposed version info & added comments

--- static/js/lib.js
+++ static/js/lib.js
@@ -1,8 +1,13 @@
 var Backend = {};
-Backend.Api = function(version){
+/**
+ * Backend.Api makes it possible to make calls to a backend in a simplified manner. It gives supplies defaults for GET, POST, PUT, PATCH and DELETE requests
+ * 
+ * @param baseUrl The base url of the backend. If you want to PUT something to https://server.io/api/object then you can supply https://server.io/api as your base url. In your request methods your can supply the /object uri. Alternatively you can omit it and supply the whole url to the request method
+ */
+Backend.Api = function(baseUrl){
 
-    var version = version || 'v1.0',
-    baseUrl = 'http://selfies.tuvok.nl/api',
+    var version = function(){return 'v1.0'},
+    baseUrl = baseUrl||'',
     defaults = { xhrFields: {withCredentials: true}, contentType: 'application/x-www-form-urlencoded', type: 'GET', success: function(data, status, xhr){console.info('API request ' + xhr.status + " " + xhr.statusText + " Bytes: " + xhr.response.length);}, error: function(xhr, errorType, error){alert('XHR call failed...');}},
     get = function(uri, r){
         request( prepareRequest(uri, r, 'GET') );
@@ -78,7 +83,8 @@ Backend.Api = function(version){
         post : post,
         put : put,
         patch : patch,
-        del : del
+        del : del,
+        version:version
     }
 };
 
